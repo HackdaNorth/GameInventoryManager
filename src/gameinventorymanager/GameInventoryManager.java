883ec748b0d02eff9ab1045cc42b88346bdfe5d5
HackdaNorth <36301205+HackdaNorth@users.noreply.github.com>
2019-07-24 12:00:08 -0400

    Group Final commit #4 Added textFields for dataBaseInteraction. Added 3 empty database files as well created file called databaseInteraction, this is for importing database files, and parsing etc.

--- src/gameinventorymanager/GameInventoryManager.java
+++ src/gameinventorymanager/GameInventoryManager.java
@@ -16,6 +16,8 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -39,15 +41,22 @@ public class GameInventoryManager extends Application {
         HBox pane = new HBox();
         Color backgroundColor = Color.web("#FBDF55");
         Color blueAccent = Color.web("#669CF5");
-        
         Scene scene = new Scene(root, 1400, 900, backgroundColor);
-
         root.getChildren().add(pane);
+
+        //new textFields for the inventory search.
+        StackPane userOptions = new StackPane();
+        userOptions.setLayoutX(600);
+        userOptions.setLayoutY(50);
+        
+        GridPane textBoxes = new GridPane();
+        textBoxes.setPadding(new Insets(10));
+        textBoxes.setHgap(10);
+        textBoxes.setVgap(10);
 
         Image imgLogo = new Image("Images/Capture3.PNG");
 
         //Image imgLogo = new Image();
-      
         ImageView iV = new ImageView(imgLogo);
         iV.setImage(imgLogo);
         iV.setFitHeight(300);
@@ -60,7 +69,8 @@ public class GameInventoryManager extends Application {
         Rectangle rec1 = new Rectangle(650, 300, blueAccent);
         rec1.setY(50);
         rec1.setX(600);
-        root.getChildren().add(rec1);
+        userOptions.getChildren().add(rec1);
+        root.getChildren().add(userOptions);
 
         Rectangle rec2 = new Rectangle(1200, 450, blueAccent);
         rec2.setY(430);
@@ -69,17 +79,101 @@ public class GameInventoryManager extends Application {
 
         Button btn = new Button();
         btn.setText("OK");
-     
+
         Button btnSearch = new Button();
         btnSearch.setText("Search");
         btnSearch.setLayoutX(100);
         btnSearch.setLayoutY(400);
+        
 
         TextField tf = new TextField();
         tf.setPromptText("Search...");
         tf.setLayoutX(160);
         tf.setLayoutY(400);
         root.getChildren().add(tf);
+        
+        
+       
+        TextField dbName = new TextField();
+        Label lbName = new Label("Name:");
+        textBoxes.add(lbName, 1, 0);
+        textBoxes.add(dbName, 1, 1);
+        dbName.setPromptText("Name...");        
+
+        Label lbCost = new Label("Cost:");
+        TextField dbCost = new TextField();
+        textBoxes.add(lbCost, 2, 0);
+        textBoxes.add(dbCost, 2, 1);
+        dbCost.setPromptText("Cost...");  
+
+        Label lbHoursPlayed = new Label("Hours played:");        
+        TextField dbHourPlayed = new TextField();
+        textBoxes.add(lbHoursPlayed, 3, 0);
+        textBoxes.add(dbHourPlayed, 3, 1);
+        dbHourPlayed.setPromptText("Hours Played...");  
+        
+
+        Label lbMP = new Label("Multiplayer");        
+        TextField dbMP = new TextField();
+        textBoxes.add(lbMP, 4, 0);
+        textBoxes.add(dbMP, 4, 1);
+        dbMP.setPromptText("Multiplayer...");  
+        
+        Label lbPublisher = new Label("Publisher");
+        TextField dbPublisher = new TextField();
+        textBoxes.add(lbPublisher, 1, 2);
+        textBoxes.add(dbPublisher, 1, 3);
+        dbPublisher.setPromptText("Publisher...");  
+        
+        Label lbDateMade = new Label("Date Made");
+        TextField dbDateMade = new TextField();
+        textBoxes.add(lbDateMade, 2, 2);        
+        textBoxes.add(dbDateMade, 2, 3);
+        dbDateMade.setPromptText("Date Made...");  
+        
+        Label lbLength = new Label("Length");
+        TextField dbLength = new TextField();
+        textBoxes.add(lbLength, 3, 2);
+        textBoxes.add(dbLength, 3, 3);
+        dbLength.setPromptText("Length..."); 
+        
+        Label lbDeveloper = new Label("Developer");
+        TextField dbDeveloper = new TextField();
+        textBoxes.add(lbDeveloper, 1, 4);
+        textBoxes.add(dbDeveloper, 1, 5);
+        dbDeveloper.setPromptText("Developer..."); 
+        
+        Label lbRating = new Label("Rating");
+        TextField dbRating = new TextField();
+        textBoxes.add(lbRating, 2, 4);       
+        textBoxes.add(dbRating, 2, 5);
+        dbRating.setPromptText("Rating..."); 
+        
+        Label lbDatePurchased = new Label("Date Purchased");
+        TextField dbDatePurchased = new TextField();
+        textBoxes.add(lbDatePurchased, 3, 4);
+        textBoxes.add(dbDatePurchased, 3, 5);
+        dbDatePurchased.setPromptText("Date Purchased..."); 
+        
+        Label lbAchievements = new Label("Achievements");
+        TextField dbAchievements = new TextField();
+        textBoxes.add(lbAchievements, 1, 6);
+        textBoxes.add(dbAchievements, 1, 7);
+        dbAchievements.setPromptText("Achievements..."); 
+        
+        Label lbGenre = new Label("Genre");
+        TextField dbGenre = new TextField();
+        textBoxes.add(lbGenre, 2, 6);
+        textBoxes.add(dbGenre, 2, 7);
+        dbGenre.setPromptText("Genre..."); 
+        
+        Label lbPlatform = new Label("Platform");
+        TextField dbPlatform = new TextField();
+        textBoxes.add(lbPlatform, 3, 6);
+        textBoxes.add(dbPlatform, 3, 7);
+        dbPlatform.setPromptText("Platform..."); 
+        
+        userOptions.getChildren().add(textBoxes);
 
 //        tf.onAction(new EventHandler<ActionEvent>() {
 //            @Override
@@ -98,8 +192,7 @@ public class GameInventoryManager extends Application {
             @Override
             public void handle(ActionEvent event) {
                 String userSearch = tf.getText();
-                
-                
+
             }
         });
 
---
+++ src/gameinventorymanager/databaseInteraction.java
@@ -0,0 +1,18 @@
+
+
+package gameinventorymanager;
+
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Conman
+ */
+public class databaseInteraction  {
+
+   
+
+    
+}
